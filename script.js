b4b8bea42a603a57a1e9de5951ebde9f5677b271
lennertverreth <lennert@verreth.be>
2019-07-25 10:54:41 +0200

    sync docs

---
+++ script.js
@@ -0,0 +1,23 @@
+/*var data = document.getElementById("table1");
+
+
+
+
+function getCountries() {
+    var countries = document.getElementById("table1").rows
+    alert(document.getElementById("table1").rows[0].cells[0].innerHTML);
+  }*/
+
+
+
+var table = document.getElementById("table1");
+
+  for (var i = 0 ; i < table.rows.length ; i++) {
+    var row = [i];
+  }
+    for (var j = 0 ; j < ){
+
+    }
+
+/*var years = document.getElementsByTagName("th");
+console.log(years);  */
